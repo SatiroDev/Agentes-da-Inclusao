5e7dc91f922f6ebf7bd6d956a48c3e307ede6e99
José Satiro <jardelsatiro57@gmail.com>
2026-01-04 12:32:56 -0300

    finaliza tela inicial e adiciona tela de upload

--- src/app.py
+++ src/app.py
@@ -1,38 +1,48 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 agentes = [
     {
+        "id": 0,
         "nome": "Agente IA - TEA",
         "descricao": "Adapta linguagem e estrutura para alunos com Transtorno do Especrtro Autista.",
-        "urlImg": ""
+        "urlImg": "puzzle-piece-svgrepo-com.svg"
     },
     {
+        "id": 1,
         "nome": "Agente IA - Surdez",
         "descricao": "Otimiza conteúdo visual e simplifica textos para alunos surdos ou com deficiência auditiva.",
-        "urlImg": ""
+        "urlImg": "ear-svgrepo-com.svg"
     },
     {
+        "id": 2,
         "nome": "Agente IA - TDAH",
         "descricao": "Divide atividades em etapas menores e adiciona elementos de engajamento.",
-        "urlImg": ""
+        "urlImg": "brain-svgrepo-com.svg"
     },
     {
+        "id": 3,
         "nome": "Agente IA - Dislexia",
         "descricao": "Adapta textos com fontes, espaçamentos e estrutura otimizados para facilitar a leitura de pessoas com dislexia.",
-        "urlImg": ""
+        "urlImg": "eye-svgrepo-com.svg"
     },
     {
+        "id": 4,
         "nome": "Agente IA - Narrador",
         "descricao": "Otimiza conteúdo visual e simplifica textos para alunos surdos ou com deficiência auditiva.",
-        "urlImg": ""
+        "urlImg": "sound-loud-svgrepo-com.svg"
     }
 ]
 
 @app.route("/")
 def index():
+    print(agentes)
     return render_template("index.html", agentes=agentes)
+
+@app.route("/tela_upload")
+def tela_upload():
+    return render_template('tela_upload.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
